bc6a8174b695a8a0bde3f4af42eae9805562856b
Hy <hullryu@gmail.com>
2019-06-25 16:45:42 -0300

    Add view,  MVC department example

--- Teste/Controllers/DepartmentsController.cs
+++ Teste/Controllers/DepartmentsController.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Teste.Models; //Add
 
 namespace Teste.Controllers
 {
     public class DepartmentsController : Controller
     {
         public IActionResult Index()
+            /* Foi criado uma lista da classe Department*/
         {
-            return View();
+            List<Department> list = new List<Department>();
+            list.Add(new Department { Id = 1, Name = "Electronics" });
+            list.Add(new Department { Id = 2, Name = "Clothes" });
+
+            return View(list);
         }
     }
 }
