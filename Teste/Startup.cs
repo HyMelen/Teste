ac5df5fdff28e773b77be0639929f4706ad8c8ef
Hy <hullryu@gmail.com>
2019-06-26 07:15:09 -0300

    CRUD Scaffolding

--- Teste/Startup.cs
+++ Teste/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Teste.Models;
 
 namespace Teste
 {
@@ -33,6 +35,9 @@ namespace Teste
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<TesteContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("TesteContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
