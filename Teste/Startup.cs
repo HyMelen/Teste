357baa3e4c937ff7bf25cc3e7e9ca11e7d4fb7b2
Hy <hullryu@gmail.com>
2019-06-26 09:14:07 -0300

    MySQL adaptation and first migration

--- Teste/Startup.cs
+++ Teste/Startup.cs
@@ -37,7 +37,8 @@ namespace Teste
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<TesteContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("TesteContext")));
+            options.UseMySql(Configuration.GetConnectionString("TesteContext"), builder =>
+            builder.MigrationsAssembly("Teste")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
