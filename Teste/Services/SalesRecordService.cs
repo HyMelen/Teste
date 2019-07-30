db4afaa130a6e461400b207f47fa5437a1e67aac
Hy <hullryu@gmail.com>
2019-07-30 19:45:12 -0300

    Implementing simple search

---
+++ Teste/Services/SalesRecordService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Teste.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Teste.Services
+{
+    public class SalesRecordService
+    {
+        private readonly TesteContext _context;
+
+        public SalesRecordService(TesteContext testeContext)
+        {
+            _context = testeContext;
+        }
+
+        public async Task<List<SalesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate) //Os parâmetros data min e max são opcionais
+        {
+            var result = from obj in _context.SalesRecord select obj;
+            if (minDate.HasValue )
+            {
+                result = result.Where(obj => obj.Date >= minDate.Value);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(obj => obj.Date <= maxDate.Value);
+            }
+
+            return await result              //Devolve uma lista limitada e ordenada por Data, juntamente com a propriedades Seller, Department. 
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x => x.Date)
+                .ToListAsync();
+        }
+
+
+
+    }
+}
